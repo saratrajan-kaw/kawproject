6589aebe8cf2bd7133d49510650bf3988119262e
Anthony pramod Manjali <anthonypramodmanjali@Anthonys-MacBook-Air.local>
2017-11-05 14:23:32 -0800

    sample code
    Sample Code

---
+++ KAWApp/Views/MainPage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace KAWApp
+{
+    public class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            const string EVENTS = "Events";
+            const string OFFERS = "Offers";
+            const string SPONSORS = "Sponsors";
+            const string PICTURES = "Pictures";
+            const string JOIN = "Join";
+
+            Page eventsPage, offersPage, sponsorsPage, picturePage,  joinPage = null;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    eventsPage = new NavigationPage(new EventsPage())
+                    {
+                        Title = EVENTS
+                    };
+
+                    offersPage = new NavigationPage(new OffersPage())
+                    {
+                        Title = OFFERS
+                    };
+                    sponsorsPage = new NavigationPage(new SponsorsPage())
+                    {
+                        Title = SPONSORS
+                    };
+                    picturePage = new NavigationPage(new PicturesPage())
+                    {
+                        Title = PICTURES
+                    };
+                    joinPage = new NavigationPage(new AboutPage())
+                    {
+                        Title = JOIN
+                    };
+                    eventsPage.Icon = "tab_feed.png";
+                    offersPage.Icon = "tab_about.png";
+                    sponsorsPage.Icon = "tab_about.png";
+                    picturePage.Icon = "tab_about.png";
+                    joinPage.Icon = "tab_about.png";
+
+                    break;
+                default:
+                    eventsPage = new EventsPage()
+                    {
+                        Title = EVENTS
+                    };
+                    offersPage = new AboutPage()
+                    {
+                        Title = OFFERS
+                    };
+                    sponsorsPage = new AboutPage()
+                    {
+                        Title =  SPONSORS
+                    };
+                    picturePage = new AboutPage()
+                    {
+                        Title =  PICTURES
+                    };
+                    joinPage = new AboutPage()
+                    {
+                        Title = JOIN
+                    };
+                    break;
+            }
+
+            Children.Add(eventsPage);
+            Children.Add(offersPage);
+            Children.Add(sponsorsPage);
+            Children.Add(picturePage);
+            Children.Add(joinPage);
+
+
+            Title = Children[0].Title;
+        }
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+            Title = CurrentPage?.Title ?? string.Empty;
+        }
+    }
+}
